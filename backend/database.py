8d993282cb7fa633699f4ac7e949724baffd50b2
neurostek <neurostek@tutamail.com>
2025-07-01 23:01:12 +0500

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,12 @@
 import sqlite3
 
 def get_connection():
-    conn = sqlite3.connect("neurostek.db")
-    return conn
+    return sqlite3.connect("neurostek.db")
 
 def init_db():
     conn = get_connection()
     cursor = conn.cursor()
-    
+
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,6 +18,6 @@ def init_db():
         tokens INTEGER DEFAULT 200
     )
     """)
-    
+
     conn.commit()
     conn.close()
